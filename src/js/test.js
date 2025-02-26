6dff402629e5dd2e8784bfc983d939a6034328ec
Sovyan Klinin <ivanivanovich1234@gmail.com>
2025-02-26 19:51:35 +0300

    git init/webpack config setup

---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+import './../styles/test.scss';
+
+alert('asdfadf');
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/js/index.js',
+        test: './src/js/test.js'
+    },
+
+    output: {
+        filename: 'js/[name]bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    performance: {
+        hints: false,
+    },
+
+    stats: {
+        children: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[hash][ext][query]',
+                },
+            },
+
+
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            chunks: ['index'],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'templates/test.html',
+            template: './src/test.html',
+            chunks: ['test'],
+        }),
+
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+    },
+
+    mode: 'development',
+};
